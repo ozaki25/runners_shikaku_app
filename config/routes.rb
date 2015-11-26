b9f1cc32c5fde9acea9bd1cc5751aa3ec9a64280
ozaki25 <fs65ugumori@yahoo.co.jp>
2015-11-26 09:24:46 +0900

    controllerを整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'categories#index'
-  resources :choices
+  resources :choices, except: %i(show)
   resources :questions
-  resources :exams
-  resources :categories
+  resources :exams, except: %i(show)
+  resources :categories, except: %i(show)
 end
